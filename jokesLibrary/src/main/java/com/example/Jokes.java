bb44044816375f2a078dbf98aa74e5685c39196f
Benjamin Earley <bearley@outsitenetworks.com>
2015-10-27 21:44:59 -0400

    Added jokes library and viewJokeActivity project

---
+++ jokesLibrary/src/main/java/com/example/Jokes.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Jokes {
+
+    public static String getJoke() {
+        return randomJoke();
+    }
+
+    private static String randomJoke() {
+        return "Joke";
+    }
+}
